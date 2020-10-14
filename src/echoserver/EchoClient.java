36d25703ba4aef591dd6f481742e7555cc2db835
JayS <smit9025@morris.umn.edu>
2020-10-14 12:18:51 -0500

    Implemented working EchoClient and EchoServer.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,41 @@
 package echoserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ConnectException;
+import java.net.Socket;
+
 public class EchoClient {
+    private static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException {
+        String server;
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        } else {
+            server = args[0];
+        }
+        try {
+            Socket socket = new Socket(server, portNumber);
+            InputStream input = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+            int Bytes;
+            while ((Bytes = System.in.read()) != -1) {
+                output.write(Bytes);
+                output.flush();
+                System.out.write(input.read());
+                System.out.flush();
+            }
+            input.close();
+            output.close();
+            socket.close();
+        } catch (ConnectException ce) {
+            System.out.println("We were unable to connect to " + server);
+            System.out.println("You should make sure the server is running.");
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,30 @@
 package echoserver;
 
+import java.io.*;
+import java.net.*;
+
 public class EchoServer {
+    private static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        try {
+            ServerSocket sock = new ServerSocket(portNumber);
+
+            while (true) {
+                Socket client = sock.accept();
+                System.out.println("Processing input. . .");
+                InputStream input = client.getInputStream();
+                OutputStream output = client.getOutputStream();
+                int bytes;
+                while ((bytes = input.read()) != -1) {
+                    output.write(bytes);
+                }
+                output.flush();
+                client.close();
+            }
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected error");
+            System.err.println(ioe);
+        }
+    }
 }
